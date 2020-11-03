84a4b81e971562b0077b450cbb27e8a89a6dbc59
Julieta Cavalli <julieta@kraken.uy>
2020-11-02 22:02:30 -0300

    Vetas/Create.

--- app/controllers/api/v1/ventas_controller.rb
+++ app/controllers/api/v1/ventas_controller.rb
@@ -1,51 +1,67 @@
 class Api::V1::VentasController < ApplicationController
 	def create
-			datos = { data: {
-									type: 'venta',
-									relationship: {
-										vehiculo: {
-											type: 'vehiculo',
-											id: '1'
-										}
-									},
-									propietario: {
-											type: 'persona',
-											data: {
-												attributes: {
-													nombre: 'Juan 2',
-													apellido: 'Pérez 2',
-													cedula: '12345678'
-												},
-											}
-									}
-								}
-							}
-		# tengo que ver como me llegan los parametros
-		# por ahora tengo el datos ese para ir probando
-		j = JSON.parse(datos.to_json)
+		# datos = { data: {
+		# 						type: "venta",
+		# 						relationship: {
+		# 							vehiculo: {
+		# 								type: "vehiculo",
+		# 								id: "1"
+		# 							}
+		# 						},
+		# 						propietario: {
+		# 							type: "persona",
+		# 							data:{
+		# 								attributes: {
+		# 									nombre: "Juan2",
+		# 									apellido: "Perez2",
+		# 									cedula: "123456-0"
+		# 								}
+		# 							}
+		# 						}
+		# 					}
+		# }
+
+		j = JSON.parse(params.to_json)
+		# nota: puede que al integrar con eli tenga que poner j['data'][0]['propietario']['data']['attributes']['cedula']
 		ci = j['data']['propietario']['data']['attributes']['cedula']
-		id = j['data']['relationship']['vehiculo']['id']
+		id = j['data']['relationship']['vehiculo']['id'].to_i
+		nombre = j['data']['propietario']['data']['attributes']['nombre']
+		apellido = j['data']['propietario']['data']['attributes']['apellido']
 		vehiculo = Vehiculo.find(id)
 		@venta = Venta.new(matricula: vehiculo.matricula, ci: ci )
+
+		propietario = Propietario.create(nombre: nombre, apellido: apellido, ci:ci )
+
+		body = { data:{ type:"vehiculo", id:id, attributes: {propietario_id: propietario.id } } }
 		if @venta.save
-			# @result = HTTParty.patch("https://.../api/rest/v1/vehiculos/:#{id}")
-			#  hacer el destroy del vehiculo
-			if @vehiculo.destroy
-				render json: {data: {
-												type:'venta',
-												attributes: @venta,
-												links: {
-													self: "http://.../api/ventas/#{id}"
+			patch_sucive = HTTParty.patch("https://tagsi-2020-sucive.herokuapp.com/vehiculos/#{id}", { headers: { "Content-Type" => "application/vnd.api+json"}, body: body.to_json })
+			d = JSON.parse(patch_sucive.to_json)
+			if d["data"].blank?
+				@venta.destroy
+				render json: {
+										error: {
+											status: 404,
+											title: 'No se pudo actualizar los datos en Sucive.'
+									}
+								}
+			else
+				if vehiculo.destroy
+					render json: {data: {
+													type:'venta',
+													attributes: @venta,
+													links: {
+														self: "http://.../api/ventas/#{id}"
+													}
 												}
 											}
-										}
-			else
-				render json: {
-					error: {
-						status: 404,
-						title: 'No se pudo eliminar el vehiculo.'
+				else
+					render json: {
+						error: {
+							status: 404,
+							title: 'No se pudo eliminar el vehiculo pero si se creó la venta.'
+					}
 				}
-			}
+				end
 			end
 		else
 			render json: {
@@ -63,6 +79,6 @@ class Api::V1::VentasController < ApplicationController
 	private
 
 	def ventas_params
-		params.require(:venta).permit(:ci, :matricula)
+		# params.require(:venta).permit(:ci, :matricula)
 	end
 end
