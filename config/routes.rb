73bfa5cc52fa59657b03aa58ad282065f6e9b2ed
Julieta Cavalli <julieta@kraken.uy>
2020-11-02 22:05:23 -0300

    Fix ruta.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :vehiculos
-      get :reporte_vehiculo, controller: :vechiculos
+      get :reporte_vehiculo, controller: :vehiculos
       resources :ventas
     end
   end
