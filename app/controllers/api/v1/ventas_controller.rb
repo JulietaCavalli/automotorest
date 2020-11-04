21edf11a46f8bb97258498388aab8cbdc569b117
Julieta Cavalli <julieta@kraken.uy>
2020-11-03 22:11:03 -0300

    Crear venta.

--- app/controllers/api/v1/ventas_controller.rb
+++ app/controllers/api/v1/ventas_controller.rb
@@ -31,46 +31,36 @@ class Api::V1::VentasController < ApplicationController
 		vehiculo = Vehiculo.find(id)
 		@venta = Venta.new(matricula: vehiculo.matricula, ci: ci )
 
-		propietario = Propietario.create(nombre: nombre, apellido: apellido, ci:ci )
+		body_propietario = { data:{ type:'persona', attributes: {nombre: nombre, apellido: apellido, cedula: ci } } }
+		propietario = HTTParty.post("https://tagsi-2020-sucive.herokuapp.com/personas", { headers: { 'Content-Type' => 'application/vnd.api+json'}, body: body_propietario.to_json })
+		propJson = JSON.parse(propietario.to_json)
 
-		body = { data:{ type:"vehiculo", id:id, attributes: {propietario_id: propietario.id } } }
+		response = HTTParty.get("https://tagsi-2020-sucive.herokuapp.com/vehiculos?filter[matricula]=#{vehiculo.matricula}")
+		r = JSON.parse(response.to_json)
+		id_sucive = r["data"][0]["id"]
+		body = { data:{ type:'vehiculo', id: id_sucive, attributes: {propietario_id: propJson['data']['id'] } } }
 		if @venta.save
-			patch_sucive = HTTParty.patch("https://tagsi-2020-sucive.herokuapp.com/vehiculos/#{id}", { headers: { "Content-Type" => "application/vnd.api+json"}, body: body.to_json })
+			patch_sucive = HTTParty.patch("https://tagsi-2020-sucive.herokuapp.com/vehiculos/#{id_sucive}", { headers: { 'Content-Type' => 'application/vnd.api+json'}, body: body.to_json })
 			d = JSON.parse(patch_sucive.to_json)
 			if d["data"].blank?
 				@venta.destroy
-				render json: {
-										error: {
-											status: 404,
-											title: 'No se pudo actualizar los datos en Sucive.'
-									}
-								}
+				render json: { error: { status: 404, title: 'No se pudo actualizar los datos en Sucive. Por Favor, inténtelo de nuevo' } }
 			else
 				if vehiculo.destroy
 					render json: {data: {
 													type:'venta',
 													attributes: @venta,
 													links: {
-														self: "http://.../api/ventas/#{id}"
+														self: request.url + "/#{id}"
 													}
 												}
 											}
 				else
-					render json: {
-						error: {
-							status: 404,
-							title: 'No se pudo eliminar el vehiculo pero si se creó la venta.'
-					}
-				}
+					render json: { error: { status: 404, title: 'No se pudo eliminar el vehiculo pero si se creó la venta.' } }
 				end
 			end
 		else
-			render json: {
-									error: {
-										status: 404,
-										title: 'No se pudo completar la venta.'
-								}
-							}
+			render json: { error: { status: 404, title: 'No se pudo completar la venta.' } }
 		end
 	end
 
