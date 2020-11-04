2fab8e474eaf05ed95978f9d973e43c027352031
Julieta Cavalli <julieta@kraken.uy>
2020-11-03 22:11:43 -0300

    Fixes en metodos de controlador de vehiculos.

--- app/controllers/api/v1/vehiculos_controller.rb
+++ app/controllers/api/v1/vehiculos_controller.rb
@@ -3,28 +3,51 @@ class Api::V1::VehiculosController < ApplicationController
 
 	def index
 		@vehiculos = Vehiculo.all
-		render json: {
-							data: {
-								type:'vehiculos',
-								links: {
-									self: 'http://.../api/vehiculos'
-								},
-								attributes: @vehiculos
-							}}
+		@vehiculosJson = @vehiculos.each_with_object([]) {|cal, array| array << cal.testMethod }
+		render json: { data: @vehiculosJson }
 	end
 
 	def reporte_vehiculo
-		# tengo que ver como agarrar la matricula aca
-		matricula = 'fgv3242'
-		#promer get al ministerio para obtener si es un vechiculo requerido
-		respuesta_requerido = HTTParty.get("http://.../v1/vehiculos-requeridos?matricula=#{matricula}")
-		j = JSON.parse(response)
-		#hay que ver si es requerido
-		#...
+		if params[:matricula].present?
+			matricula = JSON.parse(params[:matricula])
+			#primer get al ministerio para obtener si es un vechiculo requerido
+			respuesta_requerido = HTTParty.get("https://tagsi-2020-ministerio-interior.herokuapp.com/vehiculos?matricula=#{matricula}")
+			j = JSON.parse(respuesta_requerido.to_json)
 
-		#segundoo get, al sucive para obtener las deudas
-		respuesta_deudas = HTTParty.get("http://.../api/rest/v1/vehiculos?matricula=#{matricula}")
+			#segundoo get, al sucive para obtener las deudas
+			respuesta_deudas = HTTParty.get("https://tagsi-2020-sucive.herokuapp.com/vehiculos?filter[matricula]=#{matricula}&include=deudas")
+			d = JSON.parse(respuesta_deudas.to_json)
 
+			if j["data"].blank?
+				render json: {
+					data: {
+						type: "reporte-vehiculo",
+						id_sucive: d["data"][0]["id"],
+						attributes: d["data"][0]["attributes"],
+						relationships: d["included"],
+						requerido: false,
+						links: {
+							self: request.url,
+						}
+					}
+			}
+			else
+				render json: {
+						data: {
+							type: "reporte-vehiculo",
+							id_sucive: d["data"][0]["id"],
+							attributes: j["data"][0]["attributes"],
+							relationships: d["included"],
+							requerido: true,
+							links: {
+								self: request.url
+							}
+						}
+				}
+			end
+		else
+			render json: { error: { status: 404, title: 'Falta matricula.' }}
+		end
 	end
 
 	def create
@@ -33,43 +56,55 @@ class Api::V1::VehiculosController < ApplicationController
 		# 								matricula: "ahora5678",
 		# 								marca: "toyota",
 		# 								modelo: "corolla",
-		# 								ano: 2016   }
+		# 								ano: 2016,
+		#									precio:800  }
 		# 						}
 		# 				}
-
-		@vehiculo = Vehiculo.new(ventas_params)
-		# que hago con el precio?
-		@vehiculo.precio = 9000
-		@vehiculo.requerido = true
+		matricula = vehiculo_params[:matricula]
+		@vehiculo = Vehiculo.new(vehiculo_params)
 		if @vehiculo.save
-			# response = HTTParty.get("http://.../api/rest/v1/vehiculos?matricula=#{matricula}")
-			# @result = HTTParty.patch("https://.../api/rest/v1/vehiculos/:#{id}")
-			render json: {
-				data: {
-					type:'vehiculo',
-					attributes: @vehiculo,
-					links: {
-						self: 'http://localhost:3000/api/vehiculos'
+			response = HTTParty.get("https://tagsi-2020-sucive.herokuapp.com/vehiculos?filter[matricula]=#{ matricula }")
+			r = JSON.parse(response.to_json)
+			id_sucive = r["data"][0]["id"]
+			body = { data:{ type:'vehiculo', id: id, attributes: { propietario_id: 1 } } } # el propietario con id 1 es la automotora en la base del sucive
+			patch_sucive = HTTParty.patch("https://tagsi-2020-sucive.herokuapp.com/vehiculos/#{id_sucive}", { headers: { 'Content-Type' => 'application/vnd.api+json'}, body: body.to_json })
+			d = JSON.parse(patch_sucive.to_json)
+			if d["data"].blank?
+				@vehiculo.destroy
+				render json: { error: { status: 404, title: 'No se pudo actualizar los datos en Sucive. Por Favor, inténtelo de nuevo' } }
+			else
+				render json: {
+					data: {
+						type:'vehiculo',
+						attributes: @vehiculo,
+						links: {
+							self: request.url
+						}
 					}
-				}}
-		else
-			render json: {
-				error: {
-					status: 404,
-					title: 'Oops! No se pudo crear el vehiculo.'
 				}
-			}
+		else
+			render json: { error: { status: 404, title: 'Oops! No se pudo crear el vehiculo.' } }
 		end
 	end
 
+	def show
+		vehiculo = Vehiculo.find(params[:id])
+		if vehiculo.present?
+			render json:{ data:{ type:'vehiculo', id: vehiculo.id, attributes:{ matricula: vehiculo.matricula, marca: vehiculo.marca, modelo: vehiculo.modelo, ano: vehiculo.ano, precio: vehiculo.precio }, links:{self: request.url } } }
+		else
+			render json: { error: { status: 404, title: 'No se encontró vehiculo con ese id.' }}
+		end
+	end
 
 	def destroy
-
+		@vehiculo = Vehiculo.find(params[:id])
+		@vehiculo.destroy
+		render json: { success: { status: 200 }}
 	end
 
 	private
 
-	def ventas_params
+	def vehiculo_params
 		params.require(:data).require(:attributes).permit(:matricula, :marca, :modelo, :ano, :precio)
 	end
 end
